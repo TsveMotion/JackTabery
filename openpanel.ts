1d48073408f9dd2e8a603ed263c6b5fcc77a001b
Kristiyan Tsvetanov <tsvetozarkt@gmail.com>
2026-03-13 19:05:11 +0000

    Add OpenPanel analytics tracking and compress images for faster loading

---
+++ openpanel.ts
@@ -0,0 +1,9 @@
+import { OpenPanel } from '@openpanel/web';
+
+export const op = new OpenPanel({
+  clientId: 'f1a0aa35-a3ac-4db8-b95e-1372e2b53084',
+  apiUrl: 'https://openpanelapi.tsvweb.com',
+  trackScreenViews: true,
+  trackOutgoingLinks: true,
+  trackAttributes: true,
+});
